d44a192fc239bfc5f7668ac275d392562cb6931b
JIN HAN PARK <77492810+jinhan814@users.noreply.github.com>
2022-01-24 02:13:38 +0900

    .

--- data-structure/segment-tree.cpp
+++ data-structure/segment-tree.cpp
@@ -2,18 +2,25 @@
 #define fastio cin.tie(0)->sync_with_stdio(0)
 using namespace std;
 
-template<typename T = int64_t, size_t sz = 17, typename F = plus<T>>
+template<typename T, typename F>
 struct SegTree {
-	vector<T> tree; T t; F f{};
-	SegTree(T t = T()) : tree(1 << sz + 1, t), t(t) {}
-	explicit SegTree(T t, const F& f) : tree(1 << sz + 1, t), t(t), f(f) {}
-
+	const size_t sz; const T ID; F f{};
+	vector<T> tree;
+	SegTree(size_t n, T ID) : SegTree(n, ID, F{}) {}
+	explicit SegTree(size_t n, T ID, const F& f) :
+		sz(Log2(n)), ID(ID), f(f),
+		tree(sz << 1, ID) {}
+	static size_t Log2(size_t n) {
+		n--;
+		for (int i = 0; i < 5; i++) n |= n >> (1 << i);
+		return n + 1;
+	}
 	void Update(int i, T val) {
-		--i |= 1 << sz; tree[i] = val;
+		--i |= sz; tree[i] = val;
 		while (i >>= 1) tree[i] = f(tree[i << 1], tree[i << 1 | 1]);
 	}
-	T Query(int l, int r) {
-		T L = t, R = t; --l |= 1 << sz, --r |= 1 << sz;
+	T Query(int l, int r) const {
+		T L = ID, R = ID; --l |= sz, --r |= sz;
 		while (l <= r) {
 			if (l & 1) L = f(L, tree[l++]);
 			if (~r & 1) R = f(tree[r--], R);
